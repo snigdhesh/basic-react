3e82f2aa285a29b181085af4d547fa7d325fb986
naga vadlapudi <nagain2017@outlook.com>
2023-08-03 12:08:27 -0400

    Important topics:   - props: Helps to reuse a component for example ListGroup component in this project.   - You can also define "function" in interface.     This helps to pass data from child component to parent component.   - Base component with lamda expression

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import Message from "./Message";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>
+function App() {
+  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        cities={cities}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,38 +1,46 @@
 import { useState } from "react";
 
+//Interface here is like a model in java.
+interface ListGroupProps {
+  cities: string[];
+  heading: string;
+  //functionName: type of input => what it returns;
+  onSelectItem: (item:string) => void;
+}
 
-function ListGroup() {
-  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+/*You can also use this 
+  function ListGroup(props: ListGroupProps) {  
+  but you should do props.cities and props.heading within the function code.*/
+function ListGroup({ cities, heading, onSelectItem }: ListGroupProps) {
   //cities = [];
   //let selectedIndex = 0; //You can set it to -1, which means no city is selected.
-  
+
   /**
    * Note: Here we are trying to change "selectedIndex" variable, upon button click.
    * Which means, we are trying to change data (also called as state) in this function.
-   * 
+   *
    * But "selectedIndex" variable is a local variable to this "ListGroup" component.
    * So react is not aware of this variable.
-   * 
+   *
    * Whenever we are trying to change data within a component, we should tell react that
    * this component is going to have data(or state) that might change over time.
-   * 
+   *
    * To do that we need to use one of built-in functions in react called "useState"
-   * 
+   *
    */
 
   //Hook : It's a function that allows us to tap into built-in functions in react.
   //Here useState is called "stateHook"
   //Here selectedIndex is called stateVariable
-  const [selectedIndex, setSelectedIndex]=useState(-1); //-1 is the index, we are initializing to select no city.
+  const [selectedIndex, setSelectedIndex] = useState(-1); //-1 is the index, we are initializing to select no city.
   /*where 
     arr[0] //variable (selectedIndex) and 
     arr[1] //updater function
   Example: const [name, setName] = useState('');*/
 
-
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {cities.length == 0 ? <p>No cities found</p> : null}
       {/*
           But this is much cleaner way.
@@ -41,11 +49,18 @@ function ListGroup() {
           Watch https://www.youtube.com/watch?v=SqcY0GlETPk&t=154s @ 36:56 to understand how following statement works
         */}
       <ul className="list-group">
-        {cities.map((city,index) => (
+        {cities.map((city, index) => (
           <li
-            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={city}
-            onClick={()=> setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(city);
+            }}
           >
             {city}
           </li>
